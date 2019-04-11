2ff62feb8d17e2530e4e09d35ee9d650075bec4e
Alex Pedro <alextpedro@gmail.com>
2019-04-11 12:51:58 +0100

    Correction and notes.

--- selector-exercise.js
+++ selector-exercise.js
@@ -2,5 +2,5 @@
 
 var way1 = document.getElementById("first");
 var way2 = document.getElementsByTagName("p")[0];
-var way3 = document.querySelector(".special");
+var way3 = document.querySelector(".special"); //query selector only selects the first element
 var way4 = document.querySelectorAll("p")[0];
