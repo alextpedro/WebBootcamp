baa16c0ce155be6af753ea75357fe02461290b7b
Alex Pedro <alextpedro@gmail.com>
2019-04-11 12:49:40 +0100

    Selector exercise.

---
+++ selector-exercise.js
@@ -0,0 +1,6 @@
+//4 different ways to select the first paragraph
+
+var way1 = document.getElementById("first");
+var way2 = document.getElementsByTagName("p")[0];
+var way3 = document.querySelector(".special");
+var way4 = document.querySelectorAll("p")[0];
